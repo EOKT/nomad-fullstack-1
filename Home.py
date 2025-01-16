7d9b48fe4d0e48daf96b230e1c5e1c5d59c1eb47
EOKT <46835523+EOKT@users.noreply.github.com>
2025-01-17 00:12:24 +0900

    dd

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="FullstackGPT Home",
+)
+
+st.title("FullstackGPT Home")
+
+st.markdown(
+    """
+    Hello
+
+    Here are apps I made:
+
+    - [] [DocumentGPT](/DocumentGPT)
+    - [] [PrivateGPT](/PrivateGPT)
+    - [] [QuizGPT](/QuizGPT)
+
+    """
+)
